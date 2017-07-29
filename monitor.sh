b97f7472a3721bf0b6e888d4b6e985026181d563
Chetan Talwar <thechetantalwar@users.noreply.github.com>
2017-07-29 13:20:07 +0530

    Monitoring Script

---
+++ monitor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+foldername=$(date +%Y-%m-%d-%H:%M:%S)
+host=$(hostname)
+mkdir /home/ubuntu/logs/$foldername
+cd /home/ubuntu/logs/
+cd $foldername
+free >> free.log
+df >> df.log
+vmstat >> vmstat.log
+netstat >> netstat.log
+ps >> ps.log
+cd ..
+zip -r --password accenturehack $foldername.zip $foldername
+rm -rf $foldername
+aws s3 cp $foldername.zip s3://AwSome-accenture-hackforward/$host/
